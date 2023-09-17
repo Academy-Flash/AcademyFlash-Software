e3429d7f189f052bba1e04501402c9b5ba30dbf4
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-09-17 16:44:58 -0300

    Adjusted the RecentCards component

--- src/components/_ui/RecentCards/index.tsx
+++ src/components/_ui/RecentCards/index.tsx
@@ -3,16 +3,15 @@ import { BsCardText } from 'react-icons/bs'
 
 export const RecentCards = () => {
     return (
-
-        <section className="flex items-center space-x-2">
-            <div className="bg-pink-600 rounded-lg p-4 font-inter">
-                    <div className="flex items-center space-x-2">
-                            <div className="text-white text-2xl">RECENT CARDS
-                                <div className="flex items-center space-x-2">
-                                        <BsCardText size={100} className='fill-white' />
-                                </div>
-                            </div>
+        <section className="flex justify-center">
+            <div className="bg-pink-600 rounded-lg p-4 font-inter  w-[192px] h-[117px]  items-center">
+                <div className="flex items-center space-x-2">
+                    <div className="text-white text mx-auto">RECENT CARDS
+                        <div className="flex justify-center">
+                            <BsCardText size={70} className='fill-white' />
+                        </div>
                     </div>
+                </div>
             </div>
         </section>
     );
