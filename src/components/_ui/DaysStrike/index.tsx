b59ad53732cb134bb1283a9c333b66a14d6b8232
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-09-17 16:44:15 -0300

    Adjusted the DaysStrike component

--- src/components/_ui/DaysStrike/index.tsx
+++ src/components/_ui/DaysStrike/index.tsx
@@ -1,17 +1,20 @@
 import Link from 'next/link'
-import { BsCardText } from 'react-icons/bs'
+import { AiFillFire } from 'react-icons/ai'
+
 
 export const DaysStrike = () => {
     return (
-        <section className="flex items-center space-x-2">
-            <div className="bg-pink-600 rounded-lg p-4 font-inter">
-                    <div className="flex items-center space-x-2">
-                            <div className="text-white text-2xl">RECENT CARDS
-                                <div className="flex items-center space-x-2">
-                                        <BsCardText size={100} className='fill-white' />
-                                </div>
-                            </div>
+        <section className="flex items-center space-x-2 ">
+            <div className="bg-purple-600 rounded-lg p-4 font-inter w-[110px] h-[115px]" style={{ fontSize: '12px' }}> DAYS STRIKE
+                <div className="flex items-center space-x-2"> 
+                    <div className="text-white  flex items-center space-x-1"> 
+                        <div style={{ fontSize: '48px' }}>45</div>
+                        <div>
+                            <AiFillFire size={30} className='fill-red-700' />
+                        </div>
                     </div>
+                    
+                </div>
             </div>
         </section>
     );
