688edd3909463c5ccc891ac707d3672012951553
mateusvdcastro <mateus.vespasiano@unifesp.br>
2023-09-17 14:43:20 -0300

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { Group } from "../components/_ui/Group";
 import { Search } from "@/components/_ui/Search";
+import { RecentFolders } from '../components/_ui/RecentFolders';
 
 
 const prisma = new PrismaClient();
@@ -22,13 +23,17 @@ export default function Home() {
   return (
 
     <main>
-      <div className='w-full h-full justify-center relative items-center'>
+      <div className='w-full h-full flex justify-center relative items-center'>
 
         <div className='max-w-screen-lg w-full h-full flex-row relative'>
 
           <Group />
 
           <Search />
+
+          <RecentFolders />
+
+          {/*   */}
 
         </div>
 
