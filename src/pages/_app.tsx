67e1ba97312e6cc53f968c9068aea0ef40ce7049
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-09-24 00:00:11 -0300

    Changed sliders and buttons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { Layout } from '@/components/common/Layout'
 import '@/styles/globals.css'
-import '@/styles/config.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
