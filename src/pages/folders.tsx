c1d02224c8ee7d8e02a4eb16f486ce6216fe21de
Marshibs <marcosshiba@live.com>
2023-09-25 00:54:25 -0300

    gridDone

---
+++ src/pages/folders.tsx
@@ -0,0 +1,60 @@
+const list = [
+    {
+      name: '01',
+      color: 'gray',
+    },
+    {
+      name: '02',
+      color: 'gray',
+    },
+    {
+      name: '03',
+      color: 'gray',
+    },
+    {
+      name: '04',
+      color: 'gray',
+    },
+    {
+      name: '05',
+      color: 'gray',
+    },
+    {
+      name: '06',
+      color: 'gray',
+    },
+    {
+      name: '07',
+      color: 'gray',
+    },
+    {
+      name: '08',
+      color: 'gray',
+    },
+    {
+      name: '09',
+      color: 'gray',
+    },
+  ];
+  
+  const folders = () => {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <div className="grid grid-cols-3 grid-rows-3 gap-4 h-5/6 w-4/5">
+          {list.map((item, index) => (
+            <div
+              key={index}
+              className={`bg-${item.color}-500 h-full relative rounded-2xl gap-5 p-5 opacity-20 hover:opacity-50`}
+            >
+              <div className="flex justify-center items-center absolute inset-0">
+                <h1 className="text-6xl font-bold">{item.name}</h1>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default folders;
+  
