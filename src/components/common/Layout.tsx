b0fb233587d5c1e27b9675bc6cd3188c81f72f19
Eduardo Verissimo <verissimo_eduardo@hotmail.com>
2023-09-30 11:41:48 -0300

    Add profile component to the layout and footer

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -3,23 +3,34 @@
 // Pode conter o header, footer, sidebar, etc
 
 import { ReactNode } from "react"
+import { useState } from "react";
 import { Footer } from "./Footer"
 import { Header } from "./Header"
+import ProfileBox from "@/components/_ui/ProfileBox";
 
 interface LayoutInterface {
     children: ReactNode
 }
 
+
 export const Layout = ({ children }: LayoutInterface) => {
-    const pageName = children?.type.name
+  const pageName = children?.type.name
 
-    return (
-        <div className="w-[80%] m-auto h-[calc(100vh-64px)] relative overflow-y-auto pr-2">
-            {pageName != 'Signup' ? <Header /> : ''}
+  const [profileBox, setProfileBox] = useState(false); // Use useState for profile box
 
-            {children}
-            
-            {pageName != 'Signup' ? <Footer /> : ''}
-        </div>
-    )
+  function toggleProfileBox() {
+    setProfileBox(prevProfileBox => !prevProfileBox);
+  }
+
+  return (
+    <div className="w-[80%] m-auto h-[calc(100vh-64px)] relative overflow-y-auto pr-2">
+      {pageName != 'Signup' ? <Header /> : ''}
+
+      {children}
+      
+      {profileBox && (<ProfileBox onToggle={toggleProfileBox} />)}
+
+      {pageName != 'Signup' ? <Footer onToggleProfile={toggleProfileBox}/> : ''}
+    </div>
+  )
 }
