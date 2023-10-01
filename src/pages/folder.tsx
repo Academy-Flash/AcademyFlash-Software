64b819c26934f3bcb9c16395c5cfc1d5fa9dfc1e
Marshibs <marcosshiba@live.com>
2023-10-01 20:11:46 -0300

    ChangedGridStileToFitTheFolders

--- src/pages/folder.tsx
+++ src/pages/folder.tsx
@@ -2,62 +2,86 @@ const list = [
   {
     name: '01',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '02',
+    name: 'Hour',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '03',
+    name: 'Of',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '04',
+    name: 'Silence',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '05',
+    name: 'Occasionally',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '06',
+    name: 'Broken',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '07',
+    name: 'Up',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '08',
+    name: 'By',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
   {
-    name: '09',
+    name: 'Lego',
     color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB',
   },
-  // {
-  //   name: '10',
-  //   color: 'gray',
-  // }
+  {
+    name: 'Yoda',
+    color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB'
+  },
+  {
+    name: 'Death',
+    color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB'
+  },
+  {
+    name: 'Sound',
+    color: 'gray',
+    url: 'https://youtu.be/uO8SeXh_LaA?si=_iR89mhdnilSSmgB'
+  },
 ];
 
-const folder = () => {
+const Folder = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="grid grid-cols-3 gap-4 h-4/5 w-4/5">
+      <div className="grid gap-4 p-5 h-full w-full grid-cols-auto">
         {list.map((item, index) => (
-          <div
+          <a
             key={index}
-            className={`bg-${item.color}-500 h-full relative rounded-2xl gap-5 p-5 opacity-20 hover:opacity-50`}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-${item.color}-500 relative rounded-2xl p-5 opacity-20 hover:opacity-50 w-full h-full flex justify-center items-center`}
+            onClick={(e) => { e.preventDefault(); window.open(item.url, '_blank'); }} 
           >
             <div className="flex justify-center items-center absolute inset-0">
               <h1 className="text-6xl font-bold">{item.name}</h1>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
   );
 };
 
-export default folder;
+export default Folder;
