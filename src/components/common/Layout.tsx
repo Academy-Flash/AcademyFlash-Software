0cd5ece7fbcb49cccd02a976b60b04e0f680036d
Eduardo Verissimo <verissimo_eduardo@hotmail.com>
2023-10-02 13:49:37 -0300

    Transition of the profile component is done

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { Footer } from "./Footer"
 import { Header } from "./Header"
 import ProfileBox from "@/components/_ui/ProfileBox";
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 
 interface LayoutInterface {
     children: ReactNode
@@ -24,26 +24,47 @@ export const Layout = ({ children }: LayoutInterface) => {
   }
 
   return (
-
-     
     <div className="w-[80%] m-auto h-[calc(100vh-64px)] relative overflow-y-auto pr-2">
-
       {pageName !== 'Signup' ? <Header /> : ''}
 
-      <div className={`transition-opacity duration-500 ease-in ${profileBox ? 'opacity-0 hidden pointer-events-none' : 'opacity-100 pointer-events-auto'}`}>
-        {/* Conteúdo anterior */}
-        {children}
-      </div>
+      <AnimatePresence initial={false}>
+        {!profileBox ? (
+          <motion.div 
+            className="w-full h-full"
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1.5, ease: 'easeInOut' }} >
 
-      <div className={`w-full h-full transition-all duration-500 transform absolute ${profileBox ? 'translate-x-0 pointer-events-auto opacity-100' : 'translate-x-full pointer-events-none opacity-0'} z-20`}>
-        {/* Caixa de perfil */}
-        <ProfileBox onToggle={toggleProfileBox} style={{ visibility: profileBox ? 'visible' : 'hidden' }} />
-      </div>
+            {children}
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+
+      <AnimatePresence initial={false}>
+        {profileBox ? (
+          <motion.div
+            className="w-full h-full absolute z-20"
+            initial={{ opacity: 0, x: '100%' }}
+            animate={{ opacity: 1, x: '0%' }}
+            exit={{ opacity: 0, x: '100%' }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            {/* Caixa de perfil */}
+            <ProfileBox onToggle={toggleProfileBox} style={{ visibility: profileBox ? 'visible' : 'hidden' }} />
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
 
       {pageName !== 'Signup' ? <Footer onToggleProfile={toggleProfileBox}/> : ''}
-
     </div>
-
 
   )
 }
