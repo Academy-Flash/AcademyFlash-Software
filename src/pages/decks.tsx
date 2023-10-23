6038123f00b8863986f7c54ad7434f7ebf5b8b17
mateusvdcastro <mateus.vespasiano@unifesp.br>
2023-10-23 13:54:35 -0300

    Colors Changed Decks Page

--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -6,6 +6,15 @@ import { url } from 'inspector';
 
 export default function Decks() {
 
+    // const backgroundColors = [
+    //     'bg-red-300', 'bg-red-400', 'bg-red-500', 'bg-red-600', 'bg-red-700', 'bg-red-800', 'bg-red-900',
+    //     'bg-yellow-300', 'bg-yellow-400', 'bg-yellow-500', 'bg-yellow-600', 'bg-yellow-700', 'bg-yellow-800', 'bg-yellow-900',
+    // ]
+
+    const backgroundColors = [
+        'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink',
+    ]
+
     const [cards, setCards] = useState<{ question: string; answer: string }[]>([]);
 
     const [content, setContent] = useState([
@@ -64,6 +73,15 @@ export default function Decks() {
     const [newQuestion, setNewQuestion] = useState<string>('');
     const [newAnswer, setNewAnswer] = useState<string>('');
 
+    {/* Function that choose a random number*/}
+    const getRandomInt = (max: number) => {
+        return Math.floor(Math.random() * max);
+    }
+
+    {/* Function that choose a random color*/}
+    const getRandomColor = () => {
+        return backgroundColors[getRandomInt(backgroundColors.length)];
+    }
 
     const handleAddDeck = () => {
       if (newDeckTitle.trim() === '') return;
@@ -123,6 +141,7 @@ export default function Decks() {
                     className="p-2 border border-gray-400 rounded mr-2"
                     />
                     
+                    
 
                     <button
                     onClick={handleAddDeck}
@@ -136,10 +155,9 @@ export default function Decks() {
                 */}
             </div>
 
-
             <div>
                 {
-                    content.map((content) => (
+                    content.map((content, i) => (
                         <>
                         
                         <p className='font-bold text-3xl border-t-2 border-black'>{content.title}</p>
@@ -148,10 +166,10 @@ export default function Decks() {
                                 {
                                     decks.map((deck) => (
                                         <>
-                                            <div className="relative w-32 h-44 m-5">
-                                                <div className="absolute top-0 left-0 w-full h-full border-2 border-gray-500 rounded-lg p-4 transform rotate-3 bg-slate-700"></div>
-                                                <div className="absolute top-1 left-1 w-full h-full border-2 border-gray-500 rounded-lg p-4 transform rotate-2 bg-slate-600"></div>
-                                                <div className="absolute top-2 left-2 w-full h-full border-2 border-gray-500 rounded-lg p-4 transform rotate-1 bg-slate-500">
+                                            <div className="relative w-32 h-44 m-5"  style={{ cursor: 'pointer'}}>
+                                                <div className="absolute top-0 left-0 w-full h-full border-2 border-gray-500 rounded-lg p-4 transform rotate-3" style={{backgroundColor: backgroundColors[i]}}></div>
+                                                <div className="absolute top-1 left-1 w-full h-full border-2 border-gray-500 rounded-lg p-4 transform rotate-2" style={{backgroundColor: backgroundColors[i]}}></div>
+                                                <div className="absolute top-2 left-2 w-full h-full border-2 border-gray-500 rounded-lg p-4 transform rotate-1" style={{backgroundColor: backgroundColors[i]}}>
                                                     <div className='bg-red-300 relative bottom-0  rounded-lg w-full h-full '>
                                                         <p>{deck.title}</p>
                                                         <p>{deck.cardsCount}</p>
