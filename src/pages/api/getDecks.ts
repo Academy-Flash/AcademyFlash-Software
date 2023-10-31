8e27e8d37e3e0b0f43561e70ad666d8860e4513c
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-10-31 20:19:40 -0300

    Changed getDecks to normal and created a new file

--- src/pages/api/getDecks.ts
+++ src/pages/api/getDecks.ts
@@ -7,30 +7,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const community_name = req.body.com_name
-    const decks = await prisma.$queryRaw`
-        SELECT * FROM decks WHERE id IN 
-        (SELECT id_decks FROM community_decks WHERE id_community IN
-        (SELECT id FROM communities WHERE community_name = ${community_name}))
-        `
-        // const decks = await prisma.decks.findMany(
-    //   {
-    //     where: {
-    //       community_decks: {
-            
-    //       },
-    //       id: 
-    //     },
-    //     include: {
-    //       community_decks: {
-    //         where: {
-    //           id_community: community_name
-    //         }
-    //       },
-    //       communities: true
-    //     }
-    //   }
-    // );
+    const decks = await prisma.decks.findMany();
 
     res.status(200).json( decks );
   } catch (error) {
@@ -38,5 +15,5 @@ export default async function handler(
   } finally {
     await prisma.$disconnect();
   }
-  
+
 }
