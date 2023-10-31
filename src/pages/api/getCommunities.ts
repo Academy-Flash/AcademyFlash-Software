1ce251149c7453c285f5e911a798a2ae2ffbd0c2
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-10-31 11:30:32 -0300

    Added the command to get all communities

---
+++ src/pages/api/getCommunities.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from './_base';
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    
+  try {
+
+    const communities = await prisma.communities.findMany();
+    res.status(200).json( communities );
+
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao recuperar os baralhos.' });
+  } finally {
+    await prisma.$disconnect();
+  }
+  
+}
