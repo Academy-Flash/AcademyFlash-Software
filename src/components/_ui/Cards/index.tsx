6e778a7016633a560dc93622cbfb709ce26023ca
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-11-10 14:59:18 -0300

    Add card fetching and display functionality, and create CardsPage component

--- src/components/_ui/Cards/index.tsx
+++ src/components/_ui/Cards/index.tsx
@@ -1,15 +1,60 @@
-export const Cards = () => {
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+interface CardInterface {
+    question: string
+    answer: string
+    rating: number
+    difficulty: number
+    id_deck: number
+}
+
+interface CardsPageProps {
+    showAnswer: boolean;
+}
+
+export const Cards = ({ showAnswer }: CardsPageProps) => {
+
+    const router = useRouter();
+
+    const deck_name = router.query.deck_name;
+
+    const [cards, setCards] = useState<CardInterface[]>([]);
+
+    useEffect(() => {
+        async function getCards() {
+            try {
+                const response = await fetch('/api/getCards', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({deck_name: deck_name}),
+                });
+                const data = await response.json();
+                setCards(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getCards()
+    }, [deck_name])
+    
+    const randomIndex = Math.floor(Math.random() * cards.length);
+
     return (
         
-            <>            
+            <>  {/*TODO: - Buscar do banco os valores do usuário*/}          
                 <section className="text-black flex items-center space-x-1">
                     <div className="font-bold">UNIFESP</div>
                     <div className="text-gray-500">@Duduzinho</div>
                     <div className="text-gray-500">* 01 Jan 2023</div>
                 </section>
                 <section className="text-black mt-5 text-base">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo nulla doloribus nostrum, officiis laudantium quibusdam ex eveniet itaque perferendis suscipit quidem aperiam, numquam rerum! Omnis nesciunt totam repudiandae qui. Molestias.
+                    {cards.length > 0 ? cards[randomIndex].question : 'No cards found'}
                 </section>
+
+                {showAnswer && (<section className="text-black mt-5 text-base">
+                    Answer: {cards[randomIndex].answer}
+                </section>  )} 
             </>
     )
 }
