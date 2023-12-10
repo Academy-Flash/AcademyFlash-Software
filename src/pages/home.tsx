ddb8464b0ea3b2f04a011eb81128e55ef8f0a2fb
Seijin18 <marcosshiba@live.com>
2023-12-10 16:12:24 -0300

    Carousel Home Page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { Group } from "../components/_ui/Group";
+import { Group } from "@/components/_ui/Group";
 import { Decks } from "@/components/_ui/Decks";
 import { SearchDecks } from "@/components/_ui/SearchDecks";
 import logo from "/img/Ricardinho.png";
@@ -7,13 +7,14 @@ import logo from "/img/Ricardinho.png";
 import React from 'react';
 import Header from '@/components/_ui/HomePage/Header';
 import Footer from '@/components/_ui/HomePage/Footer';
+import CarouselDecks from "@/components/_ui/HomePage/Caroussel/CarousselDecks";
 
 const HomePage: React.FC = () => {
   return (
     <div className="font-sans">
       <Header />
       <main className="p-4">
-
+      <CarouselDecks />
       </main>
       <Footer />
     </div>
